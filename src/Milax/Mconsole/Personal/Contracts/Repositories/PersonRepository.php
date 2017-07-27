03ef61d6ab8743427f73d2320585f8c6e8635ed8
Vladimir Rudenko <rudenko@milax.ru>
2017-07-28 01:09:43 +1100

    Add method getCompiled

--- src/Milax/Mconsole/Personal/Contracts/Repositories/PersonRepository.php
+++ src/Milax/Mconsole/Personal/Contracts/Repositories/PersonRepository.php
@@ -6,10 +6,19 @@ interface PersonRepository
 {
     /**
      * Get person by slug
-     * 
+     *
      * @param  string $slug
      * @param  string $lang [Language key]
      * @return Milax\Mconsole\Personal|Models\Person
      */
     public function findBySlug($slug, $lang = null);
+
+    /**
+     * Get localized persons
+     *
+     * @param  Builder $query
+     *
+     * @return Collection
+     */
+    public function getCompiled($query = null);
 }
--- src/Milax/Mconsole/Personal/Repositories/PersonRepository.php
+++ src/Milax/Mconsole/Personal/Repositories/PersonRepository.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Milax\Mconsole\Personal\Repositories;
 
@@ -9,16 +9,33 @@ use Milax\Mconsole\Contracts\ContentCompiler;
 class PersonRepository extends EloquentRepository implements Repository
 {
     public $model = \Milax\Mconsole\Personal\Models\Person::class;
-    
+
     public function __construct(ContentCompiler $compiler) {
         $this->compiler = $compiler;
     }
-    
+
     public function findBySlug($slug, $lang = null)
     {
         $person = $this->query()->where('slug', $slug)->firstOrFail();
         $localized = $this->compiler->set($person)->localize($lang)->render()->get();
-        
+
         return $localized;
+    }
+
+    public function getCompiled($query = null)
+    {
+        if (is_null($query)) {
+            $query = $this->query()->enabled();
+        } else {
+            $query = $query->enabled();
+        }
+
+        $persons = $query->get();
+
+        foreach ($persons as $key => $person) {
+            $persons[$key] = $this->findBySlug($person->slug, \App::getLocale());
+        }
+
+        return $persons;
     }
 }
