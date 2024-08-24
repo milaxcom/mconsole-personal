23632c19e360f088db7916dc5fb0a84da2dfd686
Vladimir Rudenko <rudenko@arkout.ru>
2024-08-24 16:06:11 +1100

    Delete Form facade

--- src/Milax/Mconsole/Personal/assets/resources/views/personal/form.blade.php
+++ src/Milax/Mconsole/Personal/assets/resources/views/personal/form.blade.php
@@ -1,8 +1,6 @@
-@if (isset($item))
-	{!! Form::model($item, ['method' => 'PUT', 'url' => mconsole_url(sprintf('personal/%s', $item->id))]) !!}
-@else
-	{!! Form::open(['method' => 'POST', 'url' => mconsole_url('personal')]) !!}
-@endif
+<form method="POST" action="{{ mconsole_url(isset($item) ? sprintf('personal/%s', $item->id) : 'personal') }}" enctype="multipart/form-data">
+	@if (isset($item))@method('PUT')@endif
+	@csrf
 
 <div class="row">
 	<div class="col-lg-7 col-md-6">
@@ -17,7 +15,8 @@
 					<div class="form-group">
 						<label>{{ trans('mconsole::personal.form.slug') }}</label>
 						<div class="input-group">
-							{!! Form::text('slug', !is_null(Form::getValueAttribute('slug')) ? null : (isset($item) ? $item->slug : null), ['class' => 'form-control']) !!}
+							<input class="form-control {{ isset($class) ? $class : '' }}" type="text" autocomplete="off" placeholder="" name="slug" value="{{ isset($item->slug) ? $item->slug : (is_null(old('slug')) ? null : old('slug')) }}">
+
 							<span class="input-group-btn">
 								<button class="btn blue slugify" type="button">
 								<i class="fa fa-refresh fa-fw"></i> {{ trans('mconsole::personal.form.slugify') }}</button>
@@ -39,41 +38,49 @@
                                     @include('mconsole::forms.text', [
                 						'label' => trans('mconsole::personal.form.name'),
                 						'name' => 'name[' . $language->key . ']',
+										'value' => $item->name[$language->key] ?? null,
                 					])
                                     <hr />
                                     <h3>{{ trans('mconsole::personal.form.content') }}</h3>
                                     @include('mconsole::forms.text', [
                                         'label' => trans('mconsole::personal.form.position'),
                                         'name' => 'position[' . $language->key . ']',
+										'value' => $item->position[$language->key] ?? null,
                                     ])
                 					@if (app('API')->options->getByKey('textareatype') == 'ckeditor')
                                         @include('mconsole::forms.ckeditor', [
                                             'label' => trans('mconsole::personal.form.contacts'),
                                             'name' => 'contacts[' . $language->key . ']',
+											'value' => $item->contacts[$language->key] ?? null,
                                         ])
                                         @include('mconsole::forms.ckeditor', [
                                             'label' => trans('mconsole::personal.form.preview'),
                     						'name' => 'preview[' . $language->key . ']',
+											'value' => $item->preview[$language->key] ?? null,
                                         ])
                                         @include('mconsole::forms.ckeditor', [
                                             'label' => trans('mconsole::personal.form.biography'),
                     						'name' => 'biography[' . $language->key . ']',
+											'value' => $item->biography[$language->key] ?? null,
                                         ])
                                     @else
                                         @include('mconsole::forms.textarea', [
                                             'label' => trans('mconsole::personal.form.contacts'),
                                             'name' => 'contacts[' . $language->key . ']',
 											'size' => '50x4',
+											'value' => $item->contacts[$language->key] ?? null,
                                         ])
                                         @include('mconsole::forms.textarea', [
                     						'label' => trans('mconsole::personal.form.preview'),
                     						'name' => 'preview[' . $language->key . ']',
                                             'size' => '50x4',
+											'value' => $item->preview[$language->key] ?? null,
                     					])
                     					@include('mconsole::forms.textarea', [
                     						'label' => trans('mconsole::personal.form.biography'),
                     						'name' => 'biography[' . $language->key . ']',
                                             'size' => '50x15',
+											'value' => $item->biography[$language->key] ?? null,
                     					])
                                     @endif
                                     <hr />
@@ -81,10 +88,12 @@
                                     @include('mconsole::forms.text', [
     									'label' => trans('mconsole::personal.form.title'),
     									'name' => 'title[' . $language->key . ']',
+										'value' => $item->title[$language->key] ?? null,
     								])
     								@include('mconsole::forms.text', [
     									'label' => trans('mconsole::personal.form.description'),
     									'name' => 'description[' . $language->key . ']',
+										'value' => $item->description[$language->key] ?? null,
     								])
     							</div>
                             @endforeach
@@ -171,18 +180,20 @@
 				@include('mconsole::forms.text', [
 					'label' => trans('mconsole::personal.form.weight'),
 					'name' => 'weight',
+					'value' => $item->weight ?? 0,
 				])
                 @include('mconsole::forms.select', [
                     'label' => trans('mconsole::personal.form.enabled'),
                     'name' => 'enabled',
                     'type' => MconsoleFormSelectType::YesNo,
+					'value' => $item->enabled ?? null,
                 ])
 			</div>
 		</div>
 	</div>
 </div>
 
-{!! Form::close() !!}
+</form>
 
 @section('page.scripts')
     <script src="/massets/js/slugify.js" type="text/javascript"></script>
